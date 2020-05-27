3b6023745085501684912738b37ec170039750d5
chisoftltd <chisoftltd@gmail.com>
2020-05-27 12:46:14 +0200

    Random Permutations

---
+++ PythonNumPyApp/RandomPermutations.py
@@ -0,0 +1,16 @@
+# RandomPermutations By ChisoftMedia
+from numpy import random
+import numpy as np
+
+arr = np.array([11,22,33,44,55])
+
+random.shuffle(arr)
+
+print(arr)
+
+
+# Generating Permutation of Arrays
+
+arr = np.array([12,13,14,15,16])
+
+print(random.permutation(arr))
