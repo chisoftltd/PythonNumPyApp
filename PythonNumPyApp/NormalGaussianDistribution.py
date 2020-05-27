ab73313b27209b3110dfecb651a9a0d2788a934b
chisoftltd <chisoftltd@gmail.com>
2020-05-27 20:40:05 +0200

    Normal (Gaussian) Distribution

--- PythonNumPyApp/NormalGaussianDistribution.py
+++ PythonNumPyApp/NormalGaussianDistribution.py
@@ -1,4 +1,6 @@
 # NormalGaussianDistribution by ChisoftMedia
+import seaborn as sns
+import matplotlib.pyplot as plt
 from numpy import random
 
 x = random.normal(size=(2, 3))
@@ -8,4 +10,8 @@ print()
 x = random.normal(loc=1, scale=2, size=(2, 3))
 
 print(x)
+print()
 
+sns.distplot(random.normal(size=1000), hist=False)
+
+plt.show()
