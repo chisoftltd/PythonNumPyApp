9a7b88656f28db51228d414bf3d9b5ccc7b1a1bd
chisoftltd <chisoftltd@gmail.com>
2020-05-27 13:25:24 +0200

    Seaborn

---
+++ PythonNumPyApp/PythonSeaborn.py
@@ -0,0 +1,16 @@
+# PythonSeaborn by ChisoftMedia
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#sns.distplot([0,1,2,3,4,5])
+
+#plt.show()
+
+
+#sns.distplot([0,1,2,3,4,5], hist=False)
+
+#plt.show()
+
+sns.distplot([6,7,8,9,10,11,12,13,14,15], hist=False)
+
+plt.show()
