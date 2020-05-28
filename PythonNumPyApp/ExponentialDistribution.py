2eddfd0219a6b6deb0df2bfd8c51edcf5f494516
chisoftltd <chisoftltd@gmail.com>
2020-05-28 13:30:24 +0200

    ExponentialDistribution

---
+++ PythonNumPyApp/ExponentialDistribution.py
@@ -0,0 +1,12 @@
+
+# ExponentialDistribution by ChisoftMedia
+import seaborn as sns
+from numpy import random
+import matplotlib.pyplot as plt
+
+x = random.exponential(scale=2, size=(2, 3))
+print(x)
+
+sns.distplot(random.exponential(size=1000), hist=False)
+
+plt.show()
