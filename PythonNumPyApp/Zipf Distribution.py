0b37b7282c6599db98e4e99f4021d240133aab61
chisoftltd <chisoftltd@gmail.com>
2020-05-28 15:32:46 +0200

    Zipf Distribution

---
+++ PythonNumPyApp/Zipf Distribution.py
@@ -0,0 +1,12 @@
+# Zipf Distribution by ChisoftMedia
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = random.zipf(a=2, size=(2, 3))
+print(x)
+
+x = random.zipf(a=2, size=1000)
+sns.distplot(x[x<10], kde=False)
+
+plt.show()
