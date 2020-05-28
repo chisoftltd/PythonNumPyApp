13884e933249cecd5558fd36a9d98935f595e5ab
chisoftltd <chisoftltd@gmail.com>
2020-05-28 13:46:05 +0200

    RayleighDistribution

---
+++ PythonNumPyApp/RayleighDistribution.py
@@ -0,0 +1,12 @@
+# RayleighDistribution by ChisoftMedia
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = random.rayleigh(scale=2, size=(2, 3))
+
+print(x)
+
+sns.distplot(random.rayleigh(size=1000), hist=False)
+
+plt.show()
